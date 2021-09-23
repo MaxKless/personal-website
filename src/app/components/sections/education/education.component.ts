d7379b8d983a1f60cf2a717fd3d6e3a0557668d3
max.kless@googlemail.com <max.kless@googlemail.com>
2021-09-23 15:30:41 +0200

    education section animations

--- src/app/components/sections/education/education.component.ts
+++ src/app/components/sections/education/education.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { animate, style, transition, trigger } from '@angular/animations';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Observable } from 'rxjs';
+import { mapTo } from 'rxjs/operators';
+import { ScrollService } from 'src/app/services/scroll/scroll.service';
 
 @Component({
   selector: 'app-education',
   templateUrl: './education.component.html',
   styleUrls: ['./education.component.scss'],
+  animations: [
+    trigger('unroll', [
+      transition('false => true', [style({ height: '0%' }), animate('1000ms ease-in', style({ height: '100%' }))]),
+    ]),
+  ],
 })
-export class EducationComponent {
-  constructor() {}
+export class EducationComponent implements AfterViewInit {
+  hasReached$: Observable<boolean>;
+
+  @ViewChild('circle_icon') circleIcon: ElementRef;
+  constructor(private scrollService: ScrollService) {}
+
+  ngAfterViewInit(): void {
+    this.hasReached$ = this.scrollService.hasReachedHeight$(this.circleIcon.nativeElement).pipe(mapTo(true));
+  }
 }
--- src/app/components/sections/education/timeline-entry/timeline-entry.component.ts
+++ src/app/components/sections/education/timeline-entry/timeline-entry.component.ts
@@ -1,19 +1,34 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { animate, style, transition, trigger } from '@angular/animations';
+import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { Observable } from 'rxjs';
+import { mapTo } from 'rxjs/operators';
 import { BootstrapSize, ResizeService } from 'src/app/services/resize/resize.service';
+import { ScrollService } from 'src/app/services/scroll/scroll.service';
+
+const appearAnimation = [style({ opacity: 0 }), animate('1000ms ease-in', style({ opacity: 1 }))];
 
 @Component({
   selector: 'app-timeline-entry',
   templateUrl: './timeline-entry.component.html',
   styleUrls: ['./timeline-entry.component.scss'],
+  animations: [trigger('appear', [transition('false => true', appearAnimation)])],
 })
-export class TimelineEntryComponent implements OnInit {
+export class TimelineEntryComponent implements OnInit, AfterViewInit {
   @Input() side: 'left' | 'right';
   @Input() chip: string;
+
+  @ViewChild('entry') entry: ElementRef;
+
   size$: Observable<BootstrapSize>;
-  constructor(private resizeService: ResizeService) {}
+  hasReached$: Observable<boolean>;
+
+  constructor(private resizeService: ResizeService, private scrollService: ScrollService) {}
 
   ngOnInit(): void {
     this.size$ = this.resizeService.size$;
+  }
+
+  ngAfterViewInit(): void {
+    this.hasReached$ = this.scrollService.hasReachedHeight$(this.entry.nativeElement).pipe(mapTo(true));
   }
 }
