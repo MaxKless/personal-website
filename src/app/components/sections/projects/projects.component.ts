65d4863a9938ef1bbf1e0aef277e5875625766d7
max.kless@googlemail.com <max.kless@googlemail.com>
2021-09-27 12:34:44 +0200

    simple project appear animation

--- src/app/components/sections/projects/projects.component.ts
+++ src/app/components/sections/projects/projects.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { animate, query, stagger, style, transition, trigger } from '@angular/animations';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Observable } from 'rxjs';
+import { mapTo } from 'rxjs/operators';
+import { ScrollService } from 'src/app/services/scroll/scroll.service';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss'],
+  animations: [
+    trigger('appear', [
+      transition('false => true', [
+        query('.project', [
+          style({ opacity: 0 }),
+          stagger('150ms', [animate('500ms 200ms ease-in'), style({ opacity: 1 })]),
+        ]),
+      ]),
+    ]),
+  ],
 })
-export class ProjectsComponent {
-  constructor() {}
+export class ProjectsComponent implements AfterViewInit {
+  hasReached$: Observable<boolean>;
+
+  @ViewChild('sectionHeader') sectionHeader: ElementRef;
+
+  constructor(private scrollService: ScrollService) {}
+
+  ngAfterViewInit(): void {
+    this.hasReached$ = this.scrollService.hasReachedHeight$(this.sectionHeader.nativeElement).pipe(mapTo(true));
+  }
 }
