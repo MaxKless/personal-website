d7a1e81ffe3d9fd159e13b284352661a10b9cecf
max.kless@googlemail.com <max.kless@googlemail.com>
2021-09-29 11:05:10 +0200

    bouncy arrow animation

--- src/app/components/sections/header/header.component.ts
+++ src/app/components/sections/header/header.component.ts
@@ -1,14 +1,30 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
+  animations: [
+    trigger('downUp', [
+      transition(':increment', [
+        animate('1000ms ease-in', style({ transform: 'translateY(25px)' })),
+        animate('1000ms ease-out', style({ transform: 'translateY(0)' })),
+      ]),
+    ]),
+  ],
 })
 export class HeaderComponent {
+  arrowBounce$: Subject<number> = new Subject();
+  counter: number = 0;
   constructor() {}
 
   scrollToSection(name: string) {
     document.querySelector(`#${name}`).scrollIntoView();
+  }
+
+  bounce() {
+    this.arrowBounce$.next(this.counter++);
   }
 }
