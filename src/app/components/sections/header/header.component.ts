bcf251b2c192e1617fb96b1e41b894f3c1a0be79
max.kless@googlemail.com <max.kless@googlemail.com>
2021-09-29 12:12:24 +0200

    dynamic spacer

--- src/app/components/sections/header/header.component.ts
+++ src/app/components/sections/header/header.component.ts
@@ -1,6 +1,8 @@
 import { animate, style, transition, trigger } from '@angular/animations';
-import { Component } from '@angular/core';
-import { Subject } from 'rxjs';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { fromEvent, Observable, Subject } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+import { getTopY } from '../../../helpers';
 
 @Component({
   selector: 'app-header',
@@ -9,16 +11,27 @@ import { Subject } from 'rxjs';
   animations: [
     trigger('downUp', [
       transition(':increment', [
-        animate('1000ms ease-in', style({ transform: 'translateY(25px)' })),
+        animate('1000ms ease-in', style({ transform: 'translateY(20px)' })),
         animate('1000ms ease-out', style({ transform: 'translateY(0)' })),
       ]),
     ]),
   ],
 })
-export class HeaderComponent {
+export class HeaderComponent implements AfterViewInit {
   arrowBounce$: Subject<number> = new Subject();
   counter: number = 0;
+  spacerHeight$: Observable<number>;
+
+  @ViewChild('spacer') spacer: ElementRef;
+
   constructor() {}
+
+  ngAfterViewInit() {
+    this.spacerHeight$ = fromEvent(window, 'resize').pipe(
+      startWith(0),
+      map(() => window.innerHeight - getTopY(this.spacer.nativeElement))
+    );
+  }
 
   scrollToSection(name: string) {
     document.querySelector(`#${name}`).scrollIntoView();
